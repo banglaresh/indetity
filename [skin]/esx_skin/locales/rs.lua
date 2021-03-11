ed4ddcb176d17aedf159f09d0d6bd395ca12b5f9
banglaresh <80489465+banglaresh@users.noreply.github.com>
2021-03-11 22:42:50 +0100

    Add files via upload

---
+++ [skin]/esx_skin/locales/rs.lua
@@ -0,0 +1,9 @@
+Locales['rs'] = {
+  ['skin_menu'] = 'Skin Meni',
+  ['use_rotate_view'] = 'koristi ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ i ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ za rotiranje prikaza.',
+  ['skin'] = 'promeni skin',
+  ['saveskin'] = 'sacuvaj skin u datoteku',
+  ['confirm_escape'] = 'da li zelite da otkazete skin changer? Sve promene ostaju nespremljene.',
+  ['no'] = 'ne',
+  ['yes'] = 'da',
+}
